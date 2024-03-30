bb181b6f2aefed3e27d2f489c484406099b4c16c
Utkarsh Joshi <91935124+ReviveElixr@users.noreply.github.com>
2024-03-30 19:51:05 +0530

    Update main.dart
    changing function name from login to LoginPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,20 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:gccloud/login.dart';
+import 'LoginPage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GC Cloud',
+      title: 'Login App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: loginPage(),
+      home: LoginPage(),
     );
   }
 }
-
